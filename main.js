ef9e6291821f70135e216e3e6b1c58f83af8bfc6
Gisela Flores <102491449+GiselaFlores@users.noreply.github.com>
2023-03-15 20:15:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -29,13 +29,25 @@ if (text !== ""){
 
 function addDeleteBtn () {
 
-  const deleteBtn = document.querySelector("button");
+  const deleteBtn = document.createElement("button");
 
   deleteBtn.textContent = "X";
   deleteBtn.className = "btn-delete";
 
-  Función.
-
+  deleteBtn.addEventListener("click", (e)=>{
+  
+    const item = e.target.parentElement;
+    
+    ul.removeChild(item);
+    
+    const items = document.querySelectorAll("li");
+    
+    if(items.length === 0) {
+                              empty.style.display = "block"
+}
+  });
+ 
+  return deleteBtn;
 
 }
 
